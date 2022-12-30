a7bae1e72eaf0fc8e65be1273cbc7bc02a9058ca
AniqSafr <92425395+AniqSafr@users.noreply.github.com>
2022-12-30 12:11:53 +0800

    Added PutraCab project

---
+++ putra_cab/lib/AllScreen/mainScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class mainScreen extends StatefulWidget {
+  const mainScreen({super.key});
+
+  static const String idScreen = "mainScreen";
+  @override
+  State<mainScreen> createState() => _mainScreen();
+}
+
+class _mainScreen extends State<mainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ putra_cab/lib/AllScreen/registrationScreen.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:putra_cab/AllScreen/loginScreen.dart';
+
+class registrationScreen extends StatelessWidget {
+  const registrationScreen({super.key});
+
+  static const String idScreen = "register";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 45.0,
+              ),
+              Image(
+                image: AssetImage("gambar PutraCab/logo with name.png"),
+                width: 280.0,
+                height: 150.0,
+                alignment: Alignment.topCenter,
+              ),
+              SizedBox(
+                height: 0.1,
+              ),
+              Text(
+                "Register as a Student",
+                style: TextStyle(
+                    fontSize: 34.0,
+                    backgroundColor: Colors.pink[800],
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    SizedBox(
+                      height: 10.1,
+                    ),
+                    TextField(
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        labelText: "Name",
+                        labelStyle: TextStyle(fontSize: 14.0),
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                      style: TextStyle(fontSize: 14.0),
+                    ),
+                    SizedBox(
+                      height: 10.1,
+                    ),
+                    TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: "Email Student",
+                        labelStyle: TextStyle(fontSize: 14.0),
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                      style: TextStyle(fontSize: 14.0),
+                    ),
+                    SizedBox(
+                      height: 10.1,
+                    ),
+                    TextField(
+                      keyboardType: TextInputType.phone,
+                      decoration: InputDecoration(
+                        labelText: "No Phone",
+                        labelStyle: TextStyle(fontSize: 14.0),
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                      style: TextStyle(fontSize: 14.0),
+                    ),
+                    SizedBox(
+                      height: 10.1,
+                    ),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        labelStyle: TextStyle(fontSize: 14.0),
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                      style: TextStyle(fontSize: 14.0),
+                    ),
+                    SizedBox(
+                      height: 10.1,
+                    ),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.pink[800]),
+                          padding: MaterialStateProperty.all(
+                              const EdgeInsets.all(15)),
+                          textStyle: MaterialStateProperty.all(const TextStyle(
+                              fontSize: 14, color: Colors.white))),
+                      onPressed: () {
+                        print("Loggedin button clicked");
+                      },
+                      child: Container(
+                        height: 35.0,
+                        child: Center(
+                            child: const Text(
+                          'Register',
+                          style: TextStyle(fontSize: 18.0),
+                        )),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pushNamedAndRemoveUntil(
+                        context, loginScreen.idScreen, (route) => false);
+                  },
+                  child: Text("Already have an account? Login here")),
+              SizedBox(
+                height: 0.1,
+              ),
+              Image(
+                image: AssetImage("gambar PutraCab/bottom.png"),
+                width: 580.0,
+                height: 200.0,
+                alignment: Alignment.bottomCenter,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
